93c503595fc585721989a79576e7039c395fa38e
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-07-20 23:27:11 -0400

    added download script

---
+++ data/download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
+wget http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2
+bzip2 -d dlib_face_recognition_resnet_model_v1.dat.bz2
